bb67c9ea5aa9984e88d825c99dd902a1e42d9969
Bart Kamphorst <bart.kamphorst@tno.nl>
2025-10-09 12:08:05 +0200

    feat: allow configuration of routeBasePath

--- plugins/webpack-terms-replace-loader/index.js
+++ plugins/webpack-terms-replace-loader/index.js
@@ -4,6 +4,16 @@ const pkgUp = require('pkg-up');
 const pkg = pkgUp.sync({ cwd: process.cwd() });
 const root = process.platform === 'win32' ? path.win32.dirname(pkg) : path.dirname(pkg);
 
+function sanitize(p) {
+  const p_unix = p
+    .replace(/^\.\//, '')
+    .replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  const p_win = p.replace(/\//g, "\\")
+    .replace(/\./, "")
+    .replace(/[*+?^${}()|[\]\\]/g, '\\$&');
+  return process.platform === 'win32' ? p_win : p_unix;
+}
+
 module.exports = function(source) {
   const urlsRegex = /(?<!!)\[[^\]]+\]\([^)]+\)/g;
   const urlRegex =  /\[\s*(.*?)\s*\]\((.*?)\)/s;
@@ -22,14 +32,11 @@ import Term from "${ this.query.termPreviewComponentPath || "@grnet/docusaurus-t
         const rel_path = process.platform === 'win32' ? path.win32.relative(root, this.resourcePath) : path.relative(root, this.resourcePath);
         const pathName = new URL(urlPath, `http://bla.com/${rel_path}`).pathname;
         if (pathName.includes(this.query.termsDir.replace(/\./, ''))) {
-          const docsDir = '/docs';
-          const routeBasePath = '/documentation';
-
+          const docsDir = "/" + sanitize(this.query.docsDir);
           const pathRelativeToDocsDir = path.relative(docsDir, pathName);
-          const targetPathName = path.posix.join(
-            routeBasePath,
-            pathRelativeToDocsDir
-          );
+
+          const routeBasePath = "/" + sanitize(this.query.routeBasePath || this.query.docsDir);
+          const targetPathName = path.posix.join(routeBasePath, pathRelativeToDocsDir);
 
           const termKey =
             this.query.baseUrl.replace(/\/$/, '') +
